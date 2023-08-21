e6a314602150ac4cd0402882e239a38da25c1826
Neila <neilamumi@hotmail.fr>
2023-08-21 12:03:23 +0200

    builtinsTermine

--- export.c
+++ export.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Probook <Probook@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 13:10:31 by nmuminov          #+#    #+#             */
+/*   Updated: 2023/08/21 12:02:49 by Probook          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 void	env_modifie(char **env, int argc, char *var)
@@ -28,12 +40,44 @@ int ft_strisalpha(char *s)
     return 1;
 }
 
-void	ft_export(int argc, char **argv, char *var, char **env)
+int	ft_export(int argc, char **argv, char *var, char **env, char *name, char *value) 
 {
+	char *var;
+    size_t name_len;
+    size_t value_len;
+    size_t total_len;
+
+	name_len = ft_strlen(name);
+	value_len = ft_strlen(value);
+	total_len = name_len + value_len + 2;
 	if (argc == 2)
-		return (env_modifie(env, argc, var));
-	else if (argc == 3)
+		env_modifie(env, argc, var);
+	else if (argc == 4) 
 		fail("too many args");
-	if (ft_strisalpha(argv[3]) != 0)
+	else if (ft_strisalpha(argv[3]) != 0)
 		fail("only digit plz");
+	else if (argc == 3)
+	{
+    	*var = malloc(total_len);
+    	if (var == NULL) 
+			return (NULL);
+    	ft_strcpy(var, name);
+   		var[name_len] = '=';
+    	ft_strcpy(var + name_len + 1, value); 
+    	var[total_len - 1] = '\0';
+    	putenv(var);
+	}
+	exit (1);
 }
+
+// int main(int argc, char **argv, char **env) 
+// {
+// 	char *name = argv[1];
+// 	char *value = argv[2];
+// 	char *digits = argv[3];
+
+//     ft_export(argc, argv, env, name, value); ?????
+//     printf("%s%s%s\n", name, value, digits);
+
+//     return 0;
+// }
--- unset.c
+++ unset.c
@@ -1,1 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Probook <Probook@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 13:10:31 by nmuminov          #+#    #+#             */
+/*   Updated: 2023/08/21 12:01:25 by Probook          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
+
+char **env;
+
+int ft_unset(int argc, char **argv, char *name) 
+{
+    int i;
+
+    i = 0;
+    if (argc == 2)
+        fail("not enough args");
+    else if (argc > 3)
+        fail("to many args");
+    while (env[i]) 
+    {
+        if (ft_strncmp(env[i], name, ft_strlen(name)) == 0 && env[i][ft_strlen(name)] == '=') 
+        {
+            while (env[i]) 
+            {
+                ft_bzero(env[i], ft_strlen(env[i]));
+                i++;
+            }
+            break;
+        }
+        i++;
+    }
+    exit(1);
+}
